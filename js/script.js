7113d9fdba89470eb9e651b6b86c8cf4959cd80e
DavideAlovisio13 <alovisiod@gmail.com>
2024-03-18 16:51:09 +0100

    (project) add function

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
 /*
-
 Consegna
-L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-Bonus
+Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell'esercizio ma solo l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l'inizializzazione di git).
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: **nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+BONUS:
 Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
-Consigli del giorno: :party_wizard:
+difficoltà 1 ⇒ 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+difficoltà 2 ⇒ 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+difficoltà 3 ⇒ 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+Consigli del giorno:
 Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
 Ad esempio:
-Di cosa ho bisogno per generare i numeri?
-Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
+Di cosa ho bisogno per generare i numeri? Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
 Le validazioni e i controlli possiamo farli anche in un secondo momento.
-:laptop_parrot: :laptop_parrot: Buon lavoro a tutte e a tutti! 
-
 */
+
 // dichiaro le variabili che mi interessano...
 const genBtn = document.querySelector('.btn');
 const divCont = document.getElementById('contgrid');
 console.log(divCont);
 
 genBtn.addEventListener('click', function () {
+    const uniNumb = new Set();
+    let resultUniq;
     // variabile reset
     divCont.innerHTML = '';
     // variabili relative alla selezione della difficoltà
@@ -33,6 +33,7 @@ genBtn.addEventListener('click', function () {
     let valueSel = inpuSelect.options[indexSelect];
     let innerOptVal = parseInt(valueSel.value);
     // ciclo per inserire le celle di gioco all'interno del contenitore principale e i numeri all'interno delle celle
+    resultUniq = getRandUniqNumb(innerOptVal, 16);
     for (let x = 0; x < innerOptVal; x++) {
         let newPlayCells = createNewCell(x + 1);
         const textCells = document.createElement('p');
@@ -51,7 +52,7 @@ genBtn.addEventListener('click', function () {
             newPlayCells.classList.add('cell-size49');
         }
         newPlayCells.addEventListener('click', function () {
-            if (divNum(x + 1)) {
+            if (number === uniNumb) {
                 newPlayCells.classList.add('press');
             } else {
                 textCells.classList.add('d-none');
@@ -62,11 +63,7 @@ genBtn.addEventListener('click', function () {
     }
     // funzione per la divisione x 3, 5 e 15
     function divNum(number) {
-        if (number % 5 === 0) {
-            return true;
-        } else if (number % 3 === 0) {
-            return true;
-        } else if (number % 15 === 0) {
+        if (number === 0) {
             return true;
         } else {
             return false;
@@ -79,5 +76,28 @@ genBtn.addEventListener('click', function () {
         divCont.append(playCells);
         return playCells;
     }
+
+    function getRandUniqNumb (maximum, lenghtNum) {
+
+        while (uniNumb.size < lenghtNum) {
+            const casualNumb = Math.floor(Math.random() * maximum) + 1;
+            uniNumb.add(casualNumb);
+        }
+
+        return uniNumb;
+    }
+
+    console.log(resultUniq)
+
+
+
+
+
+
+
+
+
+
+
 });
 
